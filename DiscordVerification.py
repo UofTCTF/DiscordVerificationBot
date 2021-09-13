c36c0c4bde02bdea6837953ab124152847fabc73
Justin Chan <csnow.to@gmail.com>
2021-09-13 03:00:23 -0400

    Fixed channel type

--- DiscordVerification.py
+++ DiscordVerification.py
@@ -51,7 +51,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.channel.type != 'private':
+    if message.channel.type != discord.ChannelType.private:
         return
 
     user_id = str(message.author.id)
