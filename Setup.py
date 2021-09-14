4add52060ee4519b5b4582fed59c82ed3acbf302
Justin Chan <csnow.to@gmail.com>
2021-09-14 12:12:30 -0400

    Security measures to prevent DDOS and bruteforce

--- Setup.py
+++ Setup.py
@@ -13,5 +13,7 @@ if __name__ == "__main__":
     db.Table('Users', metadata,
              db.Column('id', db.String(255), nullable=False, unique=True),
              db.Column('email', db.String(255), nullable=True),
-             db.Column('code', db.String(255), nullable=False))
+             db.Column('code', db.String(255), nullable=False),
+             db.Column('resends', db.Integer, nullable=False),
+             db.Column('attempts', db.Integer, nullable=False))
     metadata.create_all(engine)
